ca3a9e755eebde1405e5c3549f6c0d24a390ea03
astora83 <62930880+astora83@users.noreply.github.com>
2020-11-22 12:08:55 -0600

    plzwork you

---
+++ rightprocs/Assets/ProcGenAssignment/LevelManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    // can't see 2D array in inspector
+    public Transform[] roomSpawnersRow0;
+    public Transform[] roomSpawnersRow1;
+    public Transform[] roomSpawnersRow2;
+    public Transform[] roomSpawnersRow3;
+
+    public GameObject[] rooms;
+
+    public int testRow = 0;
+    public int testColumn = 0;
+    public int testType = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            AddRoom(testRow, testColumn, testType);
+        }
+	}
+
+    public void AddRoom(int row, int column, int roomType)
+    {
+        Vector3 spawnPos = Vector3.zero;
+        // figure out position to spawn at
+        switch(row)
+        {
+            case 0:
+                spawnPos = roomSpawnersRow0[column].position;
+                break;
+            case 1:
+                spawnPos = roomSpawnersRow1[column].position;
+                break;
+            case 2:
+                spawnPos = roomSpawnersRow2[column].position;
+                break;
+            case 3:
+                spawnPos = roomSpawnersRow3[column].position;
+                break;
+        }
+
+        // actually spawn it
+        Instantiate(rooms[roomType], spawnPos, transform.rotation);
+    }
+}
